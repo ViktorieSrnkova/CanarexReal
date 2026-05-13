bede75be2169146168f6ca395703938d13ff2dfa
ViktorieSrnkova <81012690+ViktorieSrnkova@users.noreply.github.com>
2026-05-13 10:50:57 +0200

    canonical a href fix+ static sitemap

--- frontend-public/src/components/SEO/Canonical.tsx
+++ frontend-public/src/components/SEO/Canonical.tsx
@@ -1,15 +1,15 @@
 import { Helmet } from "react-helmet-async";
 import { useLocation, useParams } from "react-router-dom";
 
+const VITE_FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL;
 export function Canonical() {
   const { lang } = useParams();
   const location = useLocation();
 
-  const path = location.pathname.replace(/^\/(cs|en|sk)/, "");
-  const FRONTEND_URL = import.meta.env.FRONTEND_URL;
+  const path = location.pathname.replace(/^\/(cs|en|sk)/, "") || "/";
   return (
     <Helmet>
-      <link rel="canonical" href={`${FRONTEND_URL}/${lang}${path}`} />
+      <link rel="canonical" href={`${VITE_FRONTEND_URL}/${lang}${path}`} />
     </Helmet>
   );
 }
--- frontend-public/src/components/SEO/Hreflang.tsx
+++ frontend-public/src/components/SEO/Hreflang.tsx
@@ -1,20 +1,31 @@
 import { Helmet } from "react-helmet-async";
 import { useLocation } from "react-router-dom";
 
+const VITE_FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL;
 export function Hreflang() {
   const location = useLocation();
-  const path = location.pathname.replace(/^\/(cs|en|sk)/, "");
-  const FRONTEND_URL = import.meta.env.FRONTEND_URL;
-
+  const path = location.pathname.replace(/^\/(cs|en|sk)/, "") || "/";
   return (
     <Helmet>
-      <link rel="alternate" hrefLang="cs" href={`${FRONTEND_URL}/cs${path}`} />
-      <link rel="alternate" hrefLang="en" href={`${FRONTEND_URL}/en${path}`} />
-      <link rel="alternate" hrefLang="sk" href={`${FRONTEND_URL}/sk${path}`} />
+      <link
+        rel="alternate"
+        hrefLang="cs"
+        href={`${VITE_FRONTEND_URL}/cs${path}`}
+      />
+      <link
+        rel="alternate"
+        hrefLang="en"
+        href={`${VITE_FRONTEND_URL}/en${path}`}
+      />
+      <link
+        rel="alternate"
+        hrefLang="sk"
+        href={`${VITE_FRONTEND_URL}/sk${path}`}
+      />
       <link
         rel="alternate"
         hrefLang="x-default"
-        href={`${FRONTEND_URL}/cs${path}`}
+        href={`${VITE_FRONTEND_URL}/cs${path}`}
       />
     </Helmet>
   );
