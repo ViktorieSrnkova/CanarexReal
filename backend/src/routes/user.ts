e52c0850c48fdf1da4b300395729a9832b737321
ViktorieSrnkova <81012690+ViktorieSrnkova@users.noreply.github.com>
2026-06-11 20:46:09 +0200

    fixed backend route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { requireUser, type AuthRequest } from "../middleware/auth";
+import { requireUser, type AuthRequest } from "../middleware/auth.js";
 import prisma from "../lib/db";
 import bcrypt from "bcrypt";
 
